b37ea54951297bf08a06af91d61de7049272dec5
Jason Pattle <jasonpattle90@gmail.com>
2022-08-28 21:40:27 +0100

    Added a quick test using the client to fetch info and display it. Email and password removed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { authenticate, getInfo, getTimeline } from "./client/client";
+import { Pet, Photo, TimelineEntry, User } from "./client/types";
+import { getLocationDisplay } from "./client/helpers";
 
-function App() {
+// Based on - https://github.com/RMHonor/sure-pet-care
+// also for more complete reference https://github.com/benleb/surepy/blob/dev/surepy/client.py
+const email = "";
+const password = "";
+const deviceId = "site_test_device_id";
+
+type TestData = {
+  photos: Photo[];
+  user: User;
+  pets: Pet[];
+  timeline: TimelineEntry[];
+};
+
+const testPetCare = async (
+  token: string | undefined,
+  setToken: (token: string) => void,
+  setData: (data: TestData | undefined) => void
+) => {
+  let authToken = token;
+
+  if (!token) {
+    const { data, error } = await authenticate(email, password, deviceId);
+
+    if (!data || error) {
+      console.log("Error authenticating", error);
+      setData(undefined);
+      return;
+    }
+
+    authToken = data.token;
+    setToken(authToken);
+  }
+
+  if (!authToken) {
+    console.log("no idea how this happened");
+    setData(undefined);
+    return;
+  }
+
+  const { data: infoData, error: infoError } = await getInfo(authToken);
+
+  if (!infoData || infoError) {
+    console.log("Error getting info", infoError);
+    setData(undefined);
+    return;
+  }
+
+  console.log("GOT INFO", infoData);
+
+  const { data: timelineData, error: timelineError } = await getTimeline(
+    authToken
+  );
+
+  if (!timelineData || timelineError) {
+    console.log("Error getting timeline", timelineError);
+    setData(undefined);
+    return;
+  }
+
+  console.log("GOT TIMELINE", timelineData);
+
+  setData({
+    photos: infoData.photos,
+    user: infoData.user,
+    pets: infoData.pets,
+    timeline: timelineData,
+  });
+};
+
+const getPhoto = (data?: TestData, photoId?: number) =>
+  data?.photos.find((p) => p.id === photoId)?.location;
+
+const App = () => {
+  const [token, setToken] = useState<string>();
+  const [data, setData] = useState<TestData>();
+
+  const userPhoto = getPhoto(data, data?.user.photo_id);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to the SurePet Care testing platform.</p>
+        <button onClick={() => testPetCare(token, setToken, setData)}>
+          Click to test fetching data
+        </button>
+        <button onClick={() => setData(undefined)}>Click to clear</button>
+        {userPhoto && <img src={userPhoto} alt="user" />}
+        {data &&
+          data.pets.map((p) => (
+            <div>
+              <img src={p.photo.location} alt={`${p.name}`} />
+              <p>
+                {p.name} : {getLocationDisplay(p)}
+              </p>
+            </div>
+          ))}
       </header>
     </div>
   );
-}
+};
 
 export default App;
